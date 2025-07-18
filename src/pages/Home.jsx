f29880853c524b2c2309baae86c493b774d4ded8
cameronedwards02845 <cameronedwards0205@gmail.com>
2025-07-18 15:14:42 -0400

    push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ export default function Home() {
     rel="noopener noreferrer"
     // className="nav-button"
   >
-    view my resume.
+     view my resume.
   </a>
 </p>
         <h3>
--- src/pages/OtherInterests.jsx
+++ src/pages/OtherInterests.jsx
@@ -1,4 +1,4 @@
-import { useEffects } from 'react';
+import { useEffect } from 'react';
 
 export default function OtherInterests() {
   useEffect(() => {
